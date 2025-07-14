d074f635bac31a7792255d7469e73780d5ef8ec6
LouiseAkemi <lou.kuana@hotmail.com>
2025-07-14 15:25:46 -0500

    feat: adjusting menus

--- src/components/FloatingMenu.jsx
+++ src/components/FloatingMenu.jsx
@@ -1,52 +1,48 @@
 import { useState } from "react";
-import { MoreHorizontal } from "lucide-react"; 
+import { MoreHorizontal } from "lucide-react";
 import clsx from "clsx";
-import { useNavigate } from "react-router-dom";
+
+const links = [
+  {
+    label: "Open Project",
+    url: "https://github.com/LouiseKuana",
+  },
+  {
+    label: "LinkedIn",
+    url: "https://www.linkedin.com/in/louise-kuana/",
+  },
+];
 
 export default function FloatingMenu() {
   const [open, setOpen] = useState(false);
-  const navigate = useNavigate();
-
-  const handleNavigation = (label) => {
-    switch (label) {
-      case "Sign In":
-        return navigate("/signin");
-      case "Learn more":
-        return navigate("/");
-      case "Open Project":
-        return navigate("/project");
-      case "LinkedIn":
-        return window.open("https://www.linkedin.com/in/seu-perfil", "_blank"); // ou uma rota interna
-      default:
-        return;
-    }
-  };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
-      {/* flowing menu button */}
+    <div className="fixed top-18 left-10 z-50">
       <button
-        onClick={() => setOpen(!open)}
+        onClick={() => setOpen((prev) => !prev)}
         className="w-12 h-12 rounded-full shadow-lg bg-[var(--color-oxford-blue)] text-[var(--color-platinum)] hover:bg-[var(--color-oxford-blue)]/70 transition flex items-center justify-center cursor-pointer"
       >
         <MoreHorizontal size={24} />
       </button>
 
-      {/* menu animation*/}
       <div
         className={clsx(
           "mt-2 space-y-2 transition-all duration-300",
-          open ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4 pointer-events-none"
+          open
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 translate-y-4 pointer-events-none"
         )}
       >
-        {["Sign In", "Learn more", "Open Project", "LinkedIn"].map((item) => (
-          <button
-            key={item}
-            onClick={() => handleNavigation(item)}
-            className="block w-40 text-left px-4 py-2 rounded-md bg-white/10 backdrop-blur-md text-[var(--color-platinum)] hover:bg-[var(--color-delft-blue)]/10 cursor-pointer hover:text-[var(--color-delft-blue)] font-serif text-sm transition"
+        {links.map(({ label, url }) => (
+          <a
+            key={label}
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block w-40 text-left px-4 py-2 rounded-md bg-white/10 backdrop-blur-md text-[var(--color-platinum)] hover:bg-[var(--color-delft-blue)]/10 hover:text-[var(--color-delft-blue)] font-serif text-sm transition"
           >
-            {item}
-          </button>
+            {label}
+          </a>
         ))}
       </div>
     </div>
---
+++ src/components/HeaderMenu.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+
+const HeaderMenu = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    setIsAuthenticated(!!token);
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    navigate("/"); 
+  };
+
+  return (
+    <header className="w-full fixed z-50 font-serif backdrop-blur-sm text-[var(--color-delft-blue)] px-6 py-4">
+      <div className="px-4 py-3 flex justify-between items-center">
+        <Link to="/" className="shiny-text text-xl font-semibold">
+          Performly Lab
+        </Link>
+
+        <nav className="space-x-6 text-sm font-serif">
+          {isAuthenticated ? (
+            <>
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/profile">User</Link>
+              <button onClick={handleLogout}>Logout</button>
+            </>
+          ) : (
+            <>
+              <Link to="/">Home</Link>
+              <Link to="/register">Sign Up</Link>
+              <Link to="/signin">Sign In</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default HeaderMenu;
