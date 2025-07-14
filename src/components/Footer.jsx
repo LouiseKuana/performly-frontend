3a5edd1d4b1565e01e8836c81247b90776672ddb
LouiseAkemi <lou.kuana@hotmail.com>
2025-07-14 15:54:28 -0500

    feat: ajusting footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return(
         <footer className="fixed bottom-0 left-0 w-full text-[var(--color-platinum)]/70 py-6 text-center font-serif">
             <p className="text-sm">
-                &copy; {new Date().getFullYear()} Performly. Open source project.
+                &copy; {new Date().getFullYear()} Performly Lab. Open source project.
             </p>
         </footer>
     );
